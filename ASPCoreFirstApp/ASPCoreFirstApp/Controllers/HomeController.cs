6521c9935cfbc169961fa4992e113b7286282255
nfunderburgh <thunderquack205@gmail.com>
2023-07-26 16:25:10 -0700

    Project Creation

---
+++ ASPCoreFirstApp/ASPCoreFirstApp/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using ASPCoreFirstApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ASPCoreFirstApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View("Index");
+        }
+
+        public IActionResult Privacy()
+        {
+            ViewBag.Message = "Be Careful about privacy";
+            return View("Privacy");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
