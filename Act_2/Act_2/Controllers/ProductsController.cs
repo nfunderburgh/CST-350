04728b44cce576cef3ea0cc8a9c8a07f3c1ca837
nfunderburgh <thunderquack205@gmail.com>
2023-08-01 12:54:39 -0700

    Testing Controllers
    The purpose of this was an activity to test controllers and adding views

---
+++ Act_2/Act_2/Controllers/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Web;
+
+namespace Act_2.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Message()
+        {
+            return View();
+        }
+
+        public IActionResult Welcome()
+        {
+            ViewBag.name = "Shad";
+            ViewBag.secretNumber = 13;
+            return View();
+        }
+    }
+}
