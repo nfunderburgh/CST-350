72f194d879df9cfe413adb73110b477f06d50259
nfunderburgh <thunderquack205@gmail.com>
2023-08-02 17:26:55 -0700

    Adding button game

---
+++ Topic 2/AppointmentMaker/AppointmentMaker/Controllers/AppointmentsController.cs
@@ -0,0 +1,39 @@
+﻿using AppointmentMaker.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppointmentMaker.Controllers
+{
+    public class AppointmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ShowAppointmentDetails(AppointmentModel appointment)
+        {
+
+            if (IsValid(appointment) == true)
+            {
+                return View("ShowAppointmentDetails", appointment);
+            }
+            else
+            {
+                return View("DontShowDetails", appointment);
+            }
+
+        }
+
+        public bool IsValid(AppointmentModel appointment)
+        {
+            if(appointment.PatientNetWorth > 89999 && appointment.PainLevel > 5)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Topic 2/AppointmentMaker/AppointmentMaker/Models/AppointmentModel.cs
@@ -0,0 +1,70 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace AppointmentMaker.Models
+{
+    public class AppointmentModel
+    {
+        [Required]
+        [DisplayName("Patient's Full Name")]
+        [StringLength(20, MinimumLength = 4)]
+        public string patientName { get; set; }
+
+        [Required]
+        [DisplayName("Patient's Street")]
+        public string Street { get; set; }
+
+        [Required]
+        [DisplayName("Patient's City")]
+        public string City { get; set; }
+
+        [Required]
+        [StringLength(5,MinimumLength = 5)]
+        [DisplayName("Patient's Zip Code")]
+        public string ZIP { get; set; }
+
+        [Required]
+        [DisplayName("Patient's email")]
+        public string email { get; set; }
+
+        [Required]
+        [StringLength(10, MinimumLength = 10)]
+        [DisplayName("Patient's Phone Number")]
+        public string phone { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [DisplayName("Appointment Request Date")]
+        public DateTime dateTime { get; set; }
+
+       
+        [DisplayName("Patient's approximate net worth")]
+        [DataType(DataType.Currency)]
+        public decimal PatientNetWorth { get; set; }
+
+
+        [DisplayName("Primary Doctor's Last Name")]
+        public string DoctorName { get; set; }
+
+        [Required]
+        [Range(1,10)]
+        [DisplayName("Patient's perceived level of pain (1 low to 10 high)")]
+        public int PainLevel { get; set; }
+
+
+
+        public AppointmentModel(string patientName, DateTime dateTime, decimal patientNetWorth, string doctorName, int painLevel)
+        {
+            this.patientName = patientName;
+            this.dateTime = dateTime;
+            PatientNetWorth = patientNetWorth;
+            DoctorName = doctorName;
+            PainLevel = painLevel;
+        }
+
+        public AppointmentModel()
+        {
+
+        }
+    }
+}
